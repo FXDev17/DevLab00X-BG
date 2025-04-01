c01df7c69efb42bb9688fc7c7bad86c36ad1ad12
FXV <fxvisione@gmail.com>
2025-04-01 12:23:57 +0100

    Fixing CheckOV Issue

--- env/dev/infra/EC2/Pipeline.tf
+++ env/dev/infra/EC2/Pipeline.tf
@@ -47,11 +47,10 @@ resource "aws_security_group" "BG_Pipeline_SG_In" {
   dynamic "ingress" {
     for_each = var.security_groups_ingress
     content {
-        # checkov:skip=CCKV_AWS_24:Dev Projects No Need To Restrict
-
       from_port   = ingress.value.from_port
       to_port     = ingress.value.to_port
       protocol    = ingress.value.protocol
+      # checkov:skip=CCKV_AWS_24:Dev Projects No Need To Restrict
       cidr_blocks = ingress.value.cidr_blocks
       description = ingress.value.description
     }
