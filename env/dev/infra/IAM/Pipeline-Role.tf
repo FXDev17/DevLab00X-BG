274944764932cedcf35b2791970a8ee1bcfee177
FXV <fxvisione@gmail.com>
2025-04-01 18:17:03 +0100

    Fixing Pipeline Permissions Issue

--- env/dev/infra/IAM/Pipeline-Role.tf
+++ env/dev/infra/IAM/Pipeline-Role.tf
@@ -19,15 +19,37 @@ resource "aws_iam_role" "BG_Pipeline_Role" {
 data "aws_iam_policy_document" "BG_Pipeline_Permissions_Policy" {
   statement {
     actions = [
+      # EC2 Permissions
       "ec2:DescribeInstances",
       "ec2:StartInstances",
       "ec2:StopInstances",
+      "ec2:ImportKeyPair",
+      "ec2:CreateSecurityGroup",
+
+      # S3 Permissions
       "s3:ListBucket",
       "s3:GetObject",
       "s3:PutObject",
       "s3:DeleteObject",
+
+      # IAM Permissions
+      "iam:CreateRole",
+      "iam:CreatePolicy"
     ]
-    resources = ["arn:aws:s3:::devlab00-logging", "arn:aws:s3:::devlab00-logging/*"]
+    resources = [
+      # S3 resources
+      "arn:aws:s3:::devlab00-logging",
+      "arn:aws:s3:::devlab00-logging/*",
+
+      # EC2 resources
+      "arn:aws:ec2:eu-west-1:817520395860:key-pair/*",
+      "arn:aws:ec2:eu-west-1:817520395860:security-group/*",
+
+      # IAM resources
+      "arn:aws:iam::817520395860:role/BG_Pipeline_Role",
+      "arn:aws:iam::817520395860:policy/BG_Pipeline_Policy"
+    
+    ]
     condition {
       test = "StringEquals"
       variable = "ec2:ResourceTag/ManagedBy"
